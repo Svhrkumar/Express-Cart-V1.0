6cb5711dcf1bd60570304fe2186c55459f5a2293
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2021-10-05 23:14:51 +0530

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use('/api/offerUploads', offerUploadRouter);
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use('/uploads', express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
 	res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
