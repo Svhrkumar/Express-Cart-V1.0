159f51a3e6389d95b70fae9bfbadee87c2088dcb
svhrkumar544 <52437024+svhrkumar544@users.noreply.github.com>
2021-10-09 11:21:34 +0530

    Registration screen changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -130,7 +130,7 @@ const Navbar = () => {
 						</li>
 						<li style={{ marginTop: '10px' }}>
 							<Link
-								to={'/signin'}
+								to={'/register'}
 								style={{
 									margin: '15px 25px',
 									fontSize: '15px',
